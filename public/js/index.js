c9ee7c3c2ca8c856c600dea08ace5558431951c5
sadiyaj <sadiyajiru@gmail.com>
2019-02-07 14:37:26 -0600

    new design

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    //scroll to a tags in navs
+    $('nav').find("a").click(function(event){
+        event.preventDefault();
+        var $href = $(this).attr("href");
+        var $anchor = $(".")+$($href).offset();
+        window.scrollTop($anchor.left, $anchor.top);
+        return false;
+  });
+
+  //Scroll to a tags in sections
+  $('section').find("a").click(function(event){
+    event.preventDefault();
+    var $href = $(this).attr("href");
+    var $anchor = $(".")+$($href).offset();
+    window.scrollTop($anchor.left, $anchor.top);
+    return false;
+});
+
+
+})
